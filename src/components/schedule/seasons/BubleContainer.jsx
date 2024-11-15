90e75191f4fb4f9dcf04991c5f3dc02dc5e5baa0
Stanislav Fedyk <konung1109@gmail.com>
2024-11-15 23:45:11 +0100

    seasons

--- src/components/schedule/seasons/BubleContainer.jsx
+++ src/components/schedule/seasons/BubleContainer.jsx
@@ -1,12 +1,31 @@
 import './Seasons.css' 
+import lublinHerb from '../../../assets/seasons/lublin-herb.png'
 export default function BubleContainer() {
     return (
         <div className='buble'>
-            <div>
-                <h2>Lublin Buble</h2>
-                <h4>27.06.2024</h4>
+            <div className='buble-info'>
+                <img id='buble-img' src={lublinHerb} alt="" />
+                <div>
+                    <h3>Lublin Buble</h3>
+                    <p>27.06.2024</p>
+                </div>
             </div>
-    
+            <div className='buble-game'>
+                <div className='buble-time'>
+                    <p>Gm 1</p>
+                    <p>|</p>
+                    <p>Tuesday Oct 1</p>
+                </div>
+                <div className='buble-result'>   
+                        <img src={lublinHerb} alt="" />
+                        <p>HL</p>
+                        <p>@</p>
+                        
+                        <img src={lublinHerb} alt="" />
+                        <p>WC</p>
+                        <p></p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/schedule/seasons/Season2024.jsx
+++ src/components/schedule/seasons/Season2024.jsx
@@ -1,8 +1,8 @@
 import BubleContainer from "./BubleContainer"
 export default function Season2024() {
     return (
-        <section>
-            <h1>2024 Sesason Schedule</h1>
+        <section className="season-section">
+            <h1 >2024 Sesason Schedule</h1>
             <BubleContainer></BubleContainer>
         </section>
     )
